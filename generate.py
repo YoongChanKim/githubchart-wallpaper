6e6578cc1ce97ca7d572be24ac36504e6815b05a
fuzer <shtowever@gmail.com>
2017-05-04 22:52:52 +0900

    add clock

--- generate.py
+++ generate.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python3
 
 import sys
+import time
 
 html = """
 <!DOCTYPE html>
@@ -8,10 +9,14 @@ html = """
 
 <head>
     <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 </head>
 
 <body>
-    <h1>%s</h1>
+    <div class="clock">
+        <h1>%s</h1>
+        <h2>%s</h2>
+    </div>
     <div class="git">
         <img src="http://ghchart.rshah.org/%s">
     </div>
@@ -28,23 +33,33 @@ body {
     overflow: hidden;
 }
 
-h1 {
-    color: rgba(255, 255, 255, 0.8);
-    font-size: 80px;
-    font-family:sans-serif;
-
+.clock {
     position: absolute;
     text-align: center;
     left: 0;
-    top: 30%%;
+    top: 35%%;
     width: 100%%;
     text-align: center;
+}
+
+.clock h1 {
+    color: rgba(255, 255, 255, 1.0);
+    font-size: 150px;
+    margin: 0px;
+    font-family: 'Open Sans', sans-serif;
+}
+
+.clock h2 {
+    color: rgba(255, 255, 255, 0.7);
+    font-size: 50px;
+    margin: 0px;
+    font-family: 'Open Sans', sans-serif;
 }
 
 .git {
     position: absolute;
     text-align: center;
-    top: 50%%;
+    top: 73%%;
     left: 0%%;
     width: 100%%;
 }
@@ -62,10 +77,11 @@ height = sys.argv[2]
 user = sys.argv[3]
 phrases = sys.argv[4]
 
+time_format = "{}:{}".format(time.strftime("%H"), time.strftime("%M"))
+
 with open('web/wallpaper.html', 'w') as html_file:
-    html_file.write(html % (phrases, user))
+    html_file.write(html % (time_format, phrases, user))
 
 with open('web/style.css', 'w') as css_file:
     css_file.write(css % (width, height, str(int(width)-10), height))
 
-print('Finish Configuration')
--- update_background.sh
+++ update_background.sh
@@ -2,14 +2,14 @@
 screen_width="1920" #<--set here your screen's width dimension
 screen_heigth="1080" #<--set here your screen's height dimension
 username="FuZer" #<--set here your github username
-phrases="Have a nice day $username!" #<--set here your phrases
+phrases="Have a nice day $username." #<--set here your phrases
 
-interval="600" #<--set here the seconds you want to sleep till the next update
+interval="60" #<--set here the seconds you want to sleep till the next update
 
-python3 ./generate.py $screen_width $screen_heigth $username "$phrases"
 gsettings set org.gnome.desktop.background picture-uri file://$HOME/.wallpaper.png
 
 while true; do
-   gnome-web-photo --timeout=30 --mode=photo --width=$screen_width --file "web/wallpaper.html" --user-css="web/style.css"  $HOME/.wallpaper.png
+	python3 ./generate.py $screen_width $screen_heigth $username "$phrases"
+	gnome-web-photo --timeout=30 --mode=photo --width=$screen_width --file "web/wallpaper.html" --user-css="web/style.css"  $HOME/.wallpaper.png
    sleep $interval
 done
